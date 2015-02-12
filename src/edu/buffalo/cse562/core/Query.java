3611a4558f87b7628be385923e1690c07dcba0ec
Vinayak Karuppasamy <vinayak.karuppasamy@gmail.com>
2015-02-11 19:29:20 -0500

    reading sql files, Query added

---
+++ src/edu/buffalo/cse562/core/Query.java
@@ -0,0 +1,17 @@
+package edu.buffalo.cse562.core;
+
+import net.sf.jsqlparser.statement.Statement;
+
+public class Query {
+
+	private Statement query; 
+	
+	public Query(Statement query){
+		this.query = query;
+	}
+	
+	public void evaluate(){
+		
+	}
+	
+}
