67d5c2e21900d0a34e124a82660b51d002c36496
sai16vicky <saiviknesh@Sai16vickys-MacBook-Air.local>
2015-02-11 19:27:32 -0500

    CP Modified

--- src/edu/buffalo/cse562/model/CartesianProduct.java
+++ src/edu/buffalo/cse562/model/CartesianProduct.java
@@ -8,34 +8,34 @@ import java.util.List;
 import net.sf.jsqlparser.schema.Table;
 
 public class CartesianProduct {
-	
-			List<Table> mytable;
-			List<ArrayList<Tuple>> data;
-			List<Tuple> output;
-			int siz;
-			
-			public CartesianProduct(List<Table> tables)
-			{
-					mytable = tables;
-					siz = tables.size();
-					for(int i=0;i<tables.size();i++)
-							data.add(tables[i].rows);
-			}
-			
-			public void CalculateCartesianProduct(int cur, Tuple tillnow)
-			{	
-					if(cur == siz)
-					{
-							output.add(tillnow);
-							return ;
-					}
-					
-					ArrayList<Tuple> tmp = data[cur];
-					
-					for(int i=0;i<tmp.size();i++)
-							doit(cur + 1, tillnow.append(tmp[i]));
-						
-					return ;
-			}
+
+                        List<Table> mytable;
+                        List<ArrayList<Tuple>> data;
+                        List<ArrayList<Tuple>> output;
+                        int siz;
+
+                        public CartesianProduct(List<Table> tables)
+                        {
+                                        mytable = tables;
+                                        siz = tables.size();
+                                        for(int i=0;i<tables.size();i++)
+                                                        data.add(tables[i].rows);
+                        }
+                        
+                        public void CalculateCartesianProduct(int cur, ArrayList<Tuple> tillnow)
+                        {
+                                        if(cur == siz)
+                                        {
+                                                        output.add(tillnow);
+                                                        return ;
+                                        }
+
+                                        ArrayList<Tuple> tmp = data[cur];
+
+                                        for(int i=0;i<tmp.size();i++)
+                                                        doit(cur + 1, tillnow.add(tmp[i]));
+
+                                        return ;
+                        }
 }
 
