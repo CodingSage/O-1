a899f2767d2ff887d002ca5837f84bd0cc72a2ab
Vinayak Karuppasamy <vinayak.karuppasamy@gmail.com>
2015-02-11 20:50:42 -0500

    reverted reverts :)

--- src/edu/buffalo/cse562/Main.java
+++ src/edu/buffalo/cse562/Main.java
@@ -1,29 +1,45 @@
 package edu.buffalo.cse562;
 
 import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+
+import net.sf.jsqlparser.parser.CCJSqlParser;
+import net.sf.jsqlparser.statement.Statement;
+import edu.buffalo.cse562.core.DataManager;
+import edu.buffalo.cse562.query.Query;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.print("We, the members of our team, agree that we will not submit any code "
-				+ "that we have not written ourselves, share our code with anyone outside of "
-				+ "our group, or use code that we have not written ourselves as a reference.");
+		System.out
+				.print("We, the members of our team, agree that we will not submit any code "
+						+ "that we have not written ourselves, share our code with anyone outside of "
+						+ "our group, or use code that we have not written ourselves as a reference.");
 		List<File> sqlFiles = new ArrayList<File>();
-		File dataFolder = null;
 		for (int i = 0; i < args.length; i++) {
 			if (args[i].contains("--data")) {
 				i++;
-				dataFolder = new File(args[i]);
+				DataManager.getInstance().setDataPath(args[i]);
 			} else
 				sqlFiles.add(new File(args[i]));
 		}
-		if (dataFolder == null) {
-			System.out.println("Data file not present");
-			System.exit(0);
+		evaluate(sqlFiles);
+	}
+
+	private static void evaluate(List<File> sqlFiles) {
+		for (File file : sqlFiles) {
+			try {
+				FileReader reader = new FileReader(file);
+				Statement statement = null;
+				CCJSqlParser parser = new CCJSqlParser(reader);
+				while ((statement = parser.Statement()) != null) {
+					new Query(statement).evaluate();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		
-		
 	}
 }
