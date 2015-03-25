62bbb38b9a15eb9443c7f9c9ff1f87b130b64852
DI-3 <311mitun@gmail.com>
2015-03-24 20:40:11 -0400

    projection done

--- src/edu/buffalo/cse562/query/operators/ProjectionOperator.java
+++ src/edu/buffalo/cse562/query/operators/ProjectionOperator.java
@@ -1,10 +1,17 @@
 package edu.buffalo.cse562.query.operators;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import net.sf.jsqlparser.expression.DoubleValue;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.LeafValue;
+import net.sf.jsqlparser.expression.LongValue;
 import edu.buffalo.cse562.checkpoint1.ProjectionNode.Target;
 import edu.buffalo.cse562.model.Operator;
 import edu.buffalo.cse562.model.Table;
+import edu.buffalo.cse562.model.Tuple;
+import edu.buffalo.cse562.query.Evaluator;
 
 public class ProjectionOperator extends Operator {
 
@@ -27,7 +34,43 @@ public class ProjectionOperator extends Operator {
 		for(int i=0; i<siz; i++)
 		{
 				Target cur = targets.get(i);
-				res.addTableColumn(table.getColumn(cur.toString()));	
+				Expression e = cur.expr;
+				//if(e != null)
+				{
+							Evaluator eval = new Evaluator(table);
+							Table tmp = new Table();
+							while(eval.hasNext())
+							{
+									try
+									{
+											eval.next();
+											LeafValue val = eval.eval(cur.expr);
+											Tuple add = new Tuple();
+											LeafValue value ;
+											if(val instanceof LongValue){
+												value = (LongValue)val ;
+												add.insertColumn(String.valueOf(((LongValue) value).getValue()));
+											}
+											else if(val instanceof DoubleValue){
+												value = (DoubleValue)val;
+												add.insertColumn(String.valueOf(((DoubleValue)value).getValue()));
+											}
+											else add.insertColumn(val.toString());
+											
+											tmp.addRow(add);
+									}
+									catch(SQLException e1)
+									{
+											e1.printStackTrace();
+									}
+							}
+							res.addTableColumn((tmp));
+				}
+				
+//				else
+//				{
+//						res.addTableColumn(table.getColumn(cur.name));	 
+//				}
 		}
 		
 		return res;
