da8ccf941d3dd551fa9f75427d3e00ddc0d4de22
sai16vicky <saiviknesh@Sai16vickys-MacBook-Air.local>
2015-04-01 22:03:41 -0400

    added efficient join implementation

--- src/edu/buffalo/cse562/model/CalculateJoin.java
+++ src/edu/buffalo/cse562/model/CalculateJoin.java
@@ -1,7 +1,9 @@
 package edu.buffalo.cse562.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import edu.buffalo.cse562.core.DataManager;
 
@@ -45,8 +47,9 @@ public class CalculateJoin {
 	}
 
 	public Table getResultTable() {
-		return InMemoryJoin(TableLeft, TableRight); 
+		return InMemoryEfficientJoin(TableLeft, TableRight); 
 	}
+
 	public int getIndleft() {
 		return indleft;
 	}
@@ -95,14 +98,16 @@ public class CalculateJoin {
 		s.addSchema(t2.getSchema());
 		ResultTable.setSchema(s);
 		
+		Tuple tmp1, tmp2; 
+		
 		for(Tuple tupl: l1)
 		{
 				for(Tuple tupr: l2)
 				{
 						if(tupl.getValue(indleft).compareTo(tupr.getValue(indright)) == 0)
 						{
-								Tuple tmp1 = tupl;
-								Tuple tmp2 = tupr;
+								tmp1 = tupl;
+								tmp2 = tupr;
 								ResultTable.addRow(tmp1.merge(tmp1, tmp2));	
 						}
 				}
@@ -110,5 +115,62 @@ public class CalculateJoin {
 		return ResultTable;
 	}
 
+	public Table InMemoryEfficientJoin(Table t1, Table t2)
+	{
+		List<Tuple> l1 = t1.getRows();
+		List<Tuple> l2 = t2.getRows();
+	
+		ResultTable = new Table();
+		
+		Map< String, List<Tuple> > hash = new HashMap<String, List<Tuple> >();
+		
+		ResultTable.setName(t1.getName() + t2.getName());
+		
+		Schema s = new Schema();
+		s.addSchema(t1.getSchema());
+		s.addSchema(t2.getSchema());
+
+		ResultTable.setSchema(s);
+		
+		
+		for(Tuple tupl : l1)
+		{
+			if(hash.get(tupl.getValue(indleft)) == null)
+			{
+				List<Tuple> tmp = new ArrayList<Tuple>();
+				tmp.add(tupl);
+				hash.put(tupl.getValue(indleft), tmp);
+			}
+			else
+			{	
+				List<Tuple> tmp = hash.get(tupl.getValue(indleft));
+				tmp.add(tupl);
+				hash.put(tupl.getValue(indleft), tmp);
+			}
+		}	
+		
+		Tuple tmp1, tmp2; 
+		
+		for(Tuple tupr : l2)
+		{
+				tmp1 = tupr;
+				
+				if(hash.get(tupr.getValue(indright)) != null)
+				{
+					List<Tuple> tmp = hash.get(tupr.getValue(indright));
+					
+					
+					for(Tuple tupl : tmp)
+					{
+						tmp2 = tupl;
+						ResultTable.addRow(tmp2.merge(tmp2, tmp1));
+					}
+				}
+			
+		}
+		
+		return ResultTable;
+				
+	}
 	
 }
